bed43ef6fd52ad59d885c155379e0a6031bd3b6b
ijaewon <ijaewon@localhost>
2024-09-18 17:22:50 -0500

    Commit

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,20 @@
+package studio3;
+import java.util.Scanner;
+
+public class Sieve {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		// First, generate a list of integers from 1 to 40:
+				boolean[][] Pnumber = new boolean[9][4];
+				for(int i = 2; i <= Pnumber.length; i++);
+				
+				{
+					for(int i = 2; i <= Pnumber.length; i++);
+					{
+						2 * i = false;
+					}
+				}
+		
+		
+}
+}
